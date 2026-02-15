eb0274cf69453d470551cce85c3902dd265e04a4
sidharth-om <sidharthothayamangalam@gmail.com>
2026-02-15 21:37:17 +0530

    Time: 154 ms (35.04%), Space: 58.5 MB (41.03%) - LeetHub

---
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var findEvenNumbers = function(digits) {
+   let result = new Set();
+    let n = digits.length;
+
+    for (let i = 0; i < n; i++) {
+        if (digits[i] === 0) continue; // no leading zero
+        
+        for (let j = 0; j < n; j++) {
+            if (j === i) continue;
+            
+            for (let k = 0; k < n; k++) {
+                if (k === i || k === j) continue;
+                
+                if (digits[k] % 2 !== 0) continue; // must be even
+                
+                let num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                result.add(num);
+            }
+        }
+    }
+
+    return Array.from(result).sort((a, b) => a - b);
+
+};
