5d80b5ec8068e0318ccd4bd26b6723b0d99d4df4
sidharth-om <sidharthothayamangalam@gmail.com>
2026-02-15 21:09:16 +0530

    Time: 20 ms (31.81%), Space: 64.3 MB (12.92%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.js
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -3,25 +3,28 @@
  * @return {number[]}
  */
 var findErrorNums = function(nums) {
-let seen={}
-let result=[]
-
-
-for(const num of nums){
-    seen[num]=(seen[num]||0)+1
-}
-for(const obj in seen){
-    if(seen[obj]===2){
-        result.push(Number(obj))
+let seen = {};
+    let result = [];
+    
+    for (const num of nums) {
+        seen[num] = (seen[num] || 0) + 1;
     }
-}
-
-for(let i=0;i<nums.length;i++){
-    if(!nums.includes(i+1)){
-        result.push(i+1)
+    
+ 
+    for (const key in seen) {
+        if (seen[key] === 2) {
+            result[0] = Number(key);
+        }
     }
-}
+    
+    
+    for (let i = 1; i <= nums.length; i++) {
+        if (!seen[i]) {
+            result[1] = i;
+            break;
+        }
+    }
+    
+    return result;
 
-
-return result
 };
