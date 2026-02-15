70f4495f241bb213bbd8d8aa129a29c870f8df8d
sidharth-om <sidharthothayamangalam@gmail.com>
2026-02-15 21:06:34 +0530

    Time: 483 ms (9.78%), Space: 63.8 MB (18.02%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.js
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -10,7 +10,11 @@ let result=[]
 for(const num of nums){
     seen[num]=(seen[num]||0)+1
 }
-
+for(const obj in seen){
+    if(seen[obj]===2){
+        result.push(Number(obj))
+    }
+}
 
 for(let i=0;i<nums.length;i++){
     if(!nums.includes(i+1)){
@@ -18,10 +22,6 @@ for(let i=0;i<nums.length;i++){
     }
 }
 
-for(const obj in seen){
-    if(seen[obj]===2){
-        result.unshift(Number(obj))
-    }
-}
+
 return result
 };
