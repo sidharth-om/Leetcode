30373a7fca79afa43eb8a9c889988f36ceb58761
sidharth-om <sidharthothayamangalam@gmail.com>
2026-09-09 13:52:11 +0530

    Time: 6 ms (65.14%), Space: 64.6 MB (85.69%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.js
+++ 0326-power-of-three/0326-power-of-three.js
@@ -6,15 +6,7 @@ var isPowerOfThree = function(n) {
     if (n === 1) return true;
     if (n < 1 || n % 3 !== 0) return false;
 
-    // function power(num) {
-    //     if (num === 1) {
-    //         return true;
-    //     }
-
-    //     return power(num / 3);
-    // }
-
-    // return power(n);
+   
     return isPowerOfThree(n/3)
 
    
