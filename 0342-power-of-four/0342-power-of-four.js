fd3e673a1e7244804ec6d8f443a0190ee9e2e095
sidharth-om <sidharthothayamangalam@gmail.com>
2026-09-09 13:58:38 +0530

    Time: 1 ms (28.48%), Space: 55.2 MB (85%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if(n===1)return true
+    if(n%4!==0||n<1)return false
+
+    return isPowerOfFour(n/4)
+};
