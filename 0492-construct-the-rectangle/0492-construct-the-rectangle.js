8795865141cd5c7ba3eff531fad4e8c086d30839
sidharth-om <sidharthothayamangalam@gmail.com>
2026-09-14 12:27:14 +0530

    Time: 0 ms (100%), Space: 55 MB (40%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.js
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.js
@@ -7,9 +7,9 @@ var constructRectangle = function(area) {
     for(let i=Math.floor(Math.sqrt(area));i>0;i--){
        if(area%i===0){
         result.push(area/i,i)
-        break
+         return result
        }
         }
     
-    return result
+   
 };
