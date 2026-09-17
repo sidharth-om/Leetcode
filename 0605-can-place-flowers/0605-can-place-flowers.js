bfe46e566118256fa8d529c4e4483302d60d3467
sidharth-om <sidharthothayamangalam@gmail.com>
2026-09-17 16:50:54 +0530

    Time: 2 ms (61.22%), Space: 55.9 MB (60.19%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -6,7 +6,7 @@
 var canPlaceFlowers = function(flowerbed, n) {
     flowerbed.unshift(0)
     flowerbed.push(0)
-    console.log(flowerbed)
+  
 
     for(let i=1;i<flowerbed.length;i++){
          if(n===0){return true}
@@ -19,25 +19,5 @@ var canPlaceFlowers = function(flowerbed, n) {
     }
     return false
 
-    //  let i=0
-    // let left=0
-    // let right=flowerbed[i+1]
    
-
-    // while(i<=flowerbed.length){
-    //     if(flowerbed[i]===0&&left===0&&right===0){
-    //         n--
-    //         flowerbed[i]=1
-    //     }
-    //     i++
-    //     left=flowerbed[i-1]
-    //     console.log(flowerbed[i+1])
-    //     if(flowerbed[i+1]===undefined){
-    //         right=0
-    //     }else{
-    //     right=flowerbed[i+1]
-    //     }
-    //     if(n===0)return true
-    // }
-    // return false
 };
