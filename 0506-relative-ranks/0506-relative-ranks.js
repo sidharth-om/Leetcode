9d0e8dffbc7c8a7b66201966bb3b98c77437d420
sidharth-om <sidharthothayamangalam@gmail.com>
2026-09-17 12:10:54 +0530

    Time: 40 ms (10.31%), Space: 58.8 MB (53.81%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+var findRelativeRanks = function(score) {
+let arr=[]
+let seen={}
+
+for(const num of score){
+
+    seen[num]=score.indexOf(num)
+}
+
+score.sort((a,b)=>b-a)
+
+
+for(let i=0;i<score.length;i++){
+    if(i===0){
+       arr[seen[score[i]]]='Gold Medal'
+    }else if(i===1){
+         arr[seen[score[i]]]='Silver Medal'
+    }else if(i===2){
+         arr[seen[score[i]]]='Bronze Medal'
+    }else{
+         arr[seen[score[i]]]=String(i+1)
+    }
+}
+
+return arr
+
+
+
+};
