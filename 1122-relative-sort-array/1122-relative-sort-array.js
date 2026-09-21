f4d48b97e463e47e55b17cec1c8b3d22b615fa06
sidharth-om <sidharthothayamangalam@gmail.com>
2026-09-21 16:59:46 +0530

    Time: 6 ms (17.83%), Space: 56.3 MB (9.55%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.js
+++ 1122-relative-sort-array/1122-relative-sort-array.js
@@ -4,20 +4,19 @@
  * @return {number[]}
  */
 var relativeSortArray = function(arr1, arr2) {
-    let position=0
+     let result = []
 
-
-    for(let i=0;i<arr2.length;i++){
-        for(let j=0;j<arr1.length;j++){
-        
-            if(arr2[i]===arr1[j]){
-                [arr1[position],arr1[j]]=[arr1[j],arr1[position]]
-                position++
+    for (let i = 0; i < arr2.length; i++) {
+        for (let j = 0; j < arr1.length; j++) {
+            if (arr1[j] === arr2[i]) {
+                result.push(arr1[j])
             }
         }
-       
     }
 
-   return([...(arr1.filter(num=>arr2.includes(num))) , ...(arr1.filter(num=>!arr2.includes(num)).sort((a,b)=>a-b))])
+    let remaining = arr1.filter(num => !arr2.includes(num))
+    remaining.sort((a, b) => a - b)
+
+    return [...result, ...remaining]
 
 };
