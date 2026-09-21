3c36f35204408ac29a95f9a53d3e6d28a34ae213
sidharth-om <sidharthothayamangalam@gmail.com>
2026-09-21 12:28:03 +0530

    Time: 13 ms (5.1%), Space: 57.8 MB (5.1%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
+ */
+var relativeSortArray = function(arr1, arr2) {
+    let position=0
+    // arr1.sort((a,b)=>a-b)
+    // console.log(arr1)
+    for(let i=0;i<arr2.length;i++){
+        for(let j=0;j<arr1.length;j++){
+            if(arr2[i]===arr1[j]){
+                [arr1[position],arr1[j]]=[arr1[j],arr1[position]]
+                position++
+            }
+        }
+       
+    }
+   return([...(arr1.filter(num=>arr2.includes(num))) , ...(arr1.filter(num=>!arr2.includes(num)).sort((a,b)=>a-b))])
+
+    // return arr1
+};
